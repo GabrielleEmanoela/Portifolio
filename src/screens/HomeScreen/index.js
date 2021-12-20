cc68045c237ac4f3966be4555bf231a216f932d0
Gabrielle Oliveira <biela.a09@gmail.com>
2021-12-20 13:29:15 -0300

    Adicionando my page

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as  Styled from './styles';
+function Index() {
+  return (
+    <Styled.Container>
+      <Styled.Title>Hello World</Styled.Title>
+    </Styled.Container>
+  );
+}
+
+export default Index;
